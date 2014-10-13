53bde5c6f8893fb8b56ea53b049bb8013b61cf92
Katur7 <grimurk@gmail.com>
2014-10-13 18:32:29 +0000

    Test empty string

---
+++ src/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+	public static void main(String []args) {
+		org.junit.runner.JUnitCore.main("StringCalculatorTest");
+	}
+
+	@Test
+	public void test
+}
